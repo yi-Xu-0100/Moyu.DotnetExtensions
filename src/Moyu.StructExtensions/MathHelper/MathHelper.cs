9fa0b29daf6cfc4559ec09340b507a877e422a27
yi_Xu <yi_Xu@yixuju.cn>
2025-07-19 20:22:30 +0800

    feat: 补充数学扩展方法

---
+++ src/Moyu.StructExtensions/MathHelper/MathHelper.cs
@@ -0,0 +1,11 @@
+﻿// Licensed to the.NET Foundation under one or more agreements.
+// The.NET Foundation licenses this file to you under the MIT license.
+
+namespace Moyu.StructExtensions;
+
+/// <summary>
+/// 提供数学辅助方法。
+/// </summary>
+public static partial class MathHelper
+{
+}
---
+++ src/Moyu.StructExtensions/MathHelper/double.cs
@@ -0,0 +1,32 @@
+﻿// Licensed to the.NET Foundation under one or more agreements.
+// The.NET Foundation licenses this file to you under the MIT license.
+
+namespace Moyu.StructExtensions;
+
+public static partial class MathHelper
+{
+    /// <summary>
+    /// 判断两个 <see cref="double"/> 值是否在指定误差范围内相等。
+    /// </summary>
+    /// <param name="a">第一个双精度浮点数。</param>
+    /// <param name="b">第二个双精度浮点数。</param>
+    /// <param name="epsilon">允许的误差范围，默认为 1e-6。</param>
+    /// <returns>若两数差的绝对值小于 <paramref name="epsilon"/>，则返回 <c>true</c>；否则返回 <c>false</c>。</returns>
+    public static bool IsEqual(this double a, double b, double epsilon = 1e-6f) => Math.Abs(a - b) < epsilon;
+
+    /// <summary>
+    /// 返回指定 <see cref="double"/> 值的符号，考虑给定的误差范围。
+    /// </summary>
+    /// <param name="value">要判断符号的值。</param>
+    /// <param name="epsilon">用于判断是否为零的误差范围。</param>
+    /// <returns>若值接近于零（在误差范围内）则返回 <c>0</c>，否则返回 <c>1</c>（正数）或 <c>-1</c>（负数）。</returns>
+    public static int Sign(this double value, double epsilon)
+    {
+        if (Math.Abs(value) < epsilon)
+        {
+            return 0;
+        }
+
+        return value > 0 ? 1 : -1;
+    }
+}
