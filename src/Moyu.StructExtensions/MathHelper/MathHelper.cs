090adb9fa18b9ca3e7efbe6b1a4882e311666884
yi_Xu <yi_Xu@yixuju.cn>
2025-07-21 11:25:58 +0800

    fix: 修正引入的默认License信息

--- src/Moyu.StructExtensions/MathHelper/MathHelper.cs
+++ src/Moyu.StructExtensions/MathHelper/MathHelper.cs
@@ -1,7 +1,4 @@
-﻿// Licensed to the .NET Foundation under one or more agreements.
-// The .NET Foundation licenses this file to you under the MIT license.
-
-namespace Moyu.StructExtensions;
+﻿namespace Moyu.StructExtensions;
 
 /// <summary>
 /// 提供数学辅助方法。
--- src/Moyu.StructExtensions/MathHelper/float.cs
+++ src/Moyu.StructExtensions/MathHelper/float.cs
@@ -1,7 +1,4 @@
-﻿// Licensed to the .NET Foundation under one or more agreements.
-// The .NET Foundation licenses this file to you under the MIT license.
-
-namespace Moyu.StructExtensions;
+﻿namespace Moyu.StructExtensions;
 
 public static partial class MathHelper
 {
