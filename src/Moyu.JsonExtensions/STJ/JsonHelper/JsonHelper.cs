4fbb654785f16a4545e0797a9d41cf98b1858124
yi_Xu <yi_Xu@yixuju.cn>
2025-07-31 14:46:46 +0800

    refactor: 补充字段直接获取 JsonSerializerOptions

--- src/Moyu.JsonExtensions/STJ/JsonHelper/JsonHelper.cs
+++ src/Moyu.JsonExtensions/STJ/JsonHelper/JsonHelper.cs
@@ -15,6 +15,45 @@ public static partial class JsonHelper
     /// </summary>
     /// <param name="optionType">JSON 配置类型，默认使用 <see cref="JsonOptionType.EncEnumStrFields" />。</param>
     /// <returns>对应的 JSON 配置选项。</returns>
-    public static JsonSerializerOptions GetOptions(JsonOptionType optionType = JsonOptionType.EncEnumStrFields) =>
+    private static JsonSerializerOptions GetOptions(JsonOptionType optionType) =>
         JsonOptionFactory.Create(optionType);
+
+    /// <summary>
+    /// 获取启用缩进格式化输出 + Web 安全编码器(避免 HTML 注入风险)的 JSON 配置选项。
+    /// </summary>
+    public static JsonSerializerOptions IndentEncOptions =>
+        GetOptions(JsonOptionType.IndentEnc);
+
+    /// <summary>
+    /// 获取启用缩进格式化输出 + Web 安全编码器(避免 HTML 注入风险)的 JSON 配置选项。
+    /// </summary>
+    public static JsonSerializerOptions EncOnlyOptions =>
+        GetOptions(JsonOptionType.EncOnly);
+
+    /// <summary>
+    /// 获取启用编码器，并序列化字段(IncludeFields = true)的 JSON 配置选项。
+    /// </summary>
+    public static JsonSerializerOptions EncFieldsOptions =>
+        GetOptions(JsonOptionType.EncFields);
+
+    /// <summary>
+    /// 获取启用缩进格式化输出 + Web 安全编码器(避免 HTML 注入风险)，
+    /// 支持 Enum 转字符串的 JSON 配置选项。
+    /// </summary>
+    public static JsonSerializerOptions IndentEncEnumStrOptions =>
+        GetOptions(JsonOptionType.IndentEncEnumStr);
+
+    /// <summary>
+    /// 获取启用 Web 安全编码器(避免 HTML 注入风险)，
+    /// 支持 Enum 转字符串，并序列化字段(IncludeFields = true)的 JSON 配置选项。
+    /// </summary>
+    public static JsonSerializerOptions EncEnumStrFieldsOptions =>
+        GetOptions(JsonOptionType.EncEnumStrFields);
+
+    /// <summary>
+    /// 获取启用缩进格式化输出 + Web 安全编码器(避免 HTML 注入风险)，
+    /// 支持 Enum 转字符串，并序列化字段(IncludeFields = true)的 JSON 配置选项。
+    /// </summary>
+    public static JsonSerializerOptions IndentEncEnumStrFieldsOptions =>
+        GetOptions(JsonOptionType.IndentEncEnumStrFields);
 }
